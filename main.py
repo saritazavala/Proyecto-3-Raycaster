cdbe8690e42f32a037abfae162a9f01a099d8488
saritazavala <saritazg2010@gmail.om>
2020-11-29 10:26:08 -0600

    :) LISTO

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ buttonsColor = (209, 101, 44)
 onSelectedButton = (85, 180, 144)
 onSelectedTextColor = (250, 250, 250)
 textColor = (0, 0, 0)
-GRASS = (89,159,13)
+GRASS = (89, 159, 13)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 # ---------------------------------------------------------
@@ -23,6 +23,7 @@ WHITE = (255, 255, 255)
 # ---------------------------------------------------------
 back = pygame.image.load('./images/main.jpg')
 inst = pygame.image.load('./images/inst.jpg')
+pause = pygame.image.load('./images/pause.jpg')
 wall1 = pygame.image.load('./images/wall1.png')
 wall2 = pygame.image.load('./images/wall2.jpg')
 wall3 = pygame.image.load('./images/wall3.jpg')
@@ -30,8 +31,8 @@ wall4 = pygame.image.load('./images/wall4.png')
 wall5 = pygame.image.load('./images/wall5.png')
 win = pygame.image.load('./images/win.jpeg')
 lose = pygame.image.load('./images/lose.jpg')
-rock =  pygame.image.load('./images/rock.png')
-pick =  pygame.image.load('./images/pick.png')
+rock = pygame.image.load('./images/rock.png')
+pick = pygame.image.load('./images/pick.png')
 
 # Enemies
 # ---------------------------------------------------------`
@@ -150,7 +151,7 @@ class Raycaster:
                     maxhit = hity
                 tx = int(maxhit * 2.56)
                 return d, self.map[j][i], tx
-            self.screen.set_at((int(x/2), int(y/2)), WHITE)
+            self.screen.set_at((int(x / 2), int(y / 2)), WHITE)
             d += 1
 
     def draw_stake(self, x, h, tx, texture):
@@ -164,35 +165,31 @@ class Raycaster:
             self.point(x, y, c)
 
     def draw_sprite(self, sprite):
-        spriteDist = ((self.player['x'] - sprite['x'])**2 + (self.player['y'] - sprite['y'])**2) ** 0.5
+        sprite_a = atan2((sprite["y"] - self.player["y"]),
+                         (sprite["x"] - self.player["x"]))
+        sprite_d = ((self.player["x"] - sprite["x"]) ** 2 +
+                    (self.player["y"] - sprite["y"]) ** 2) ** 0.5
+        sprite_size_half = int(250 / sprite_d * 70)
+        sprite_size = sprite_size_half * 2
+        sprite_x = int(500 + (sprite_a - self.player["a"]) * 477.46 +
+                       250 - sprite_size_half)
+        sprite_y = int(250 - sprite_size_half)
 
-        spriteAngle = atan2(sprite['y'] - self.player['y'], sprite['x'] - self.player['x'])
-
-        aspectRatio = sprite["texture"].get_width() / sprite["texture"].get_height()
-        spriteHeight = (self.height / spriteDist) * 50
-        spriteWidth = spriteHeight * aspectRatio
-
-        angleRads = self.player['a'] * pi / 180
-        fovRads = self.player['fov'] * pi / 180
-
-        startX = int((self.width) + (spriteAngle - angleRads)*(self.width) / fovRads - (spriteWidth/2))
-        startY = int((self.height / 2) - (spriteHeight / 2))
-
-        for x in range(startX, int(startX + spriteWidth)):
-            for y in range(startY, int(startY + spriteHeight)):
-                if 0 < x < self.width:
-                    if self.zbuffer[ x - int(self.width)] >= spriteDist:
-                        tx = int( (x - startX) * sprite["texture"].get_width() / spriteWidth )
-                        ty = int( (y - startY) * sprite["texture"].get_height() / spriteHeight )
-                        texColor = sprite["texture"].get_at((tx, ty))
-                        if texColor[3] > 128 and texColor != SPRITE_BACKGROUND:
-                            self.screen.set_at((x,y), texColor)
-                            self.zbuffer[ x - int(self.width)] = spriteDist
+        sprite_size_pro = 128 / sprite_size
+        for x in range(sprite_x, sprite_x + sprite_size):
+            for y in range(sprite_y, sprite_y + sprite_size):
+                if 500 < x < 1000 and self.zbuffer[x - 500] <= sprite_d:
+                    tx = int((x - sprite_x) * sprite_size_pro)
+                    ty = int((y - sprite_y) * sprite_size_pro)
+                    c = sprite["texture"].get_at((tx, ty))
+                    if c != (152, 0, 136, 255):
+                        self.point(x, y, c)
+                        self.zbuffer[x - 500] = sprite_d
 
     def coords(self):
         font = pygame.font.SysFont("forte", 25, False)
         coords = "X: " + str(r.player["x"]) + "   Y: " + str(r.player["y"])
-        coords_text = font.render(coords, 1, pygame.Color(58,166,166))
+        coords_text = font.render(coords, 1, pygame.Color(58, 166, 166))
         return coords_text
 
     def render(self):
@@ -209,22 +206,25 @@ class Raycaster:
                 self.draw_stake(x, h, tx, textures[m])
 
             except:
+                self.player["x"] = 70
+                self.player["y"] = 70
+                self.player['a'] = 0
                 self.lose_action()
-
 
         for x in range(0, halfWidth, self.blocksize):
             for y in range(0, self.height, self.blocksize):
-                i = int(x/self.blocksize)
-                j = int(y/self.blocksize)
+                i = int(x / self.blocksize)
+                j = int(y / self.blocksize)
                 if self.map[j][i] != ' ':
-                    self.draw_rectangle(x/2, y/2, textures[self.map[j][i]], self.blocksize/2)
+                    self.draw_rectangle(x / 2, y / 2, textures[self.map[j][i]], self.blocksize / 2)
+
         self.point(int(self.player["x"] * 0.2) + 900, int(self.player["y"] * 0.2) + 400, (255, 255, 255))
 
         for enemy in enemies:
             self.point(enemy["x"], enemy["y"], BLACK)
             self.draw_sprite(enemy)
 
-        #self.draw_player(1000 - 256 - 128, 500 - 256)
+        # self.draw_player(1000 - 256 - 128, 500 - 256)
 
     def drawText(self, text, font, color, surface, x, y):
         textobj = font.render(text, 1, color)
@@ -234,6 +234,11 @@ class Raycaster:
 
     def text_objects(self, text, font):
         textSurface = font.render(text, True, WHITE)
+        return textSurface, textSurface.get_rect()
+
+
+    def text_objects2(self, text, font):
+        textSurface = font.render(text, True, BLACK)
         return textSurface, textSurface.get_rect()
 
     def main_menu(self):
@@ -288,6 +293,7 @@ class Raycaster:
                         isClicked = True
 
             pygame.display.update()
+            clock.tick(15)
 
     def instructionsPage(self):
         screen_img = inst
@@ -317,7 +323,7 @@ class Raycaster:
             TextRect.center = (int(self.width / 2), int(self.height / 3))
             screen.blit(TextSurf, TextRect)
 
-            TextSurf, TextRect = self.text_objects("Trata de no toparte con paredes o hienas, de lo contrario",
+            TextSurf, TextRect = self.text_objects("Trata de no toparte con paredes o cazadores, de lo contrario",
                                                    description)
             TextRect.center = (int(self.width / 2), int(self.height / 2.5))
             screen.blit(TextSurf, TextRect)
@@ -330,12 +336,16 @@ class Raycaster:
             TextRect.center = (int(self.width / 2), int(self.height / 1.75))
             screen.blit(TextSurf, TextRect)
 
+            TextSurf, TextRect = self.text_objects("o click derecho, izquierdo y scroll  ", description)
+            TextRect.center = (int(self.width / 2), int(self.height / 1.60))
+            screen.blit(TextSurf, TextRect)
+
             TextSurf, TextRect = self.text_objects("Hakuna matata", message)
-            TextRect.center = (int(self.width / 1.5), int(self.height / 1.5))
+            TextRect.center = (int(self.width / 1.5), int(self.height / 1.25))
             screen.blit(TextSurf, TextRect)
 
             TextSurf, TextRect = self.text_objects("Presiona R para regresar", return_key)
-            TextRect.center = (int(self.width / 4), int(self.height / 1.5))
+            TextRect.center = (int(self.width / 4), int(self.height / 1.25))
             screen.blit(TextSurf, TextRect)
 
             pygame.display.update()
@@ -358,8 +368,6 @@ class Raycaster:
             clock.tick(15)
             screen.blit(lose, (0, 0))
 
-
-
     def win_action(self):
         while True:
             for e in pygame.event.get():
@@ -378,7 +386,6 @@ class Raycaster:
             TextRect.center = (int(self.width / 1.25), int(self.height / 2.5))
             screen.blit(TextSurf, TextRect)
 
-
             pygame.display.update()
             clock.tick(15)
             screen.blit(win, (0, 0))
@@ -389,10 +396,36 @@ class Raycaster:
         fps_text = font.render(fps, 1, pygame.Color("black"))
         return fps_text
 
+    def pause(self):
+        while True:
+            screen_img = pause
+            screen.blit(screen_img, [-1, 0])
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
+                    exit(0)
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_p:
+                        self.start_game()
+
+
+            instruction_tittle = pygame.font.SysFont('gabriola', 50, True)
+            description = pygame.font.SysFont('couriernew', 20, False, False)
+
+            TextSurf, TextRect = self.text_objects2("P A U S A", instruction_tittle)
+            TextRect.center = (int(self.width / 2), int(self.height / 4))
+            screen.blit(TextSurf, TextRect)
+
+            TextSurf, TextRect = self.text_objects2("Vuelve a apachar P para regrsar!.", description)
+            TextRect.center = (int(self.width / 2), int(self.height / 3))
+
+            screen.blit(TextSurf, TextRect)
+
+            pygame.display.update()
+            clock.tick(15)
+
     def start_game(self):
-        paused = False
-        running = True
-        while running:
+
+        while True:
             screen.fill((0, 0, 0))
             d = 10
             for e in pygame.event.get():
@@ -402,45 +435,42 @@ class Raycaster:
                     print(e.button)
                     if e.button == 1:
                         r.player["a"] -= 0.157
-                    elif e.button == 3:
+                    if e.button == 3:
                         r.player["a"] += 0.157
-                    elif e.button == 4:
+                    if e.button == 4:
                         r.player["x"] += int(d * cos(r.player["a"]))
                         r.player["y"] += int(d * sin(r.player["a"]))
-                    elif e.button == 5:
+                    if e.button == 5:
                         r.player["x"] -= int(d * cos(r.player["a"]))
                         r.player["y"] -= int(d * sin(r.player["a"]))
 
+                    if (r.player["x"] > 390) and (r.player["y"] > 70):
+                        self.win_action()
+
                 elif e.type == pygame.KEYDOWN:
-                    if not paused:
-                        if e.key == pygame.K_a:
-                            r.player["a"] -= 0.157
-                        if e.key == pygame.K_d:
-                            r.player["a"] += 0.157
-                        if e.key == pygame.K_w:
-                            r.player["x"] += int(d * cos(r.player["a"]))
-                            r.player["y"] += int(d * sin(r.player["a"]))
-                        if e.key == pygame.K_s:
-                            r.player["x"] -= int(d * cos(r.player["a"]))
-                            r.player["y"] -= int(d * sin(r.player["a"]))
+                    if e.key == pygame.K_a:
+                        r.player["a"] -= 0.157
+                    if e.key == pygame.K_d:
+                        r.player["a"] += 0.157
+                    if e.key == pygame.K_w:
+                        r.player["x"] += int(d * cos(r.player["a"]))
+                        r.player["y"] += int(d * sin(r.player["a"]))
+                    if e.key == pygame.K_s:
+                        r.player["x"] -= int(d * cos(r.player["a"]))
+                        r.player["y"] -= int(d * sin(r.player["a"]))
 
+                    if e.key == pygame.K_p:
+                        self.pause()
 
-
-                        if (r.player["x"] > 390) and (r.player["y"] > 70):
-                            self.win_action()
-
+                    if (r.player["x"] > 390) and (r.player["y"] > 70):
+                        self.win_action()
 
             screen.fill(pygame.Color("LIGHTSKYBLUE"), (0, 0, int(r.width), int(r.height / 2)))
             screen.fill(GRASS, (0, int(r.height / 2), int(r.width), int(r.height / 2)))
             r.render()
-            screen.blit(self.update_fps(), (10, 10))
-            clock.tick(30)
+            screen.blit(self.update_fps(), (745, 15))
             screen.blit(self.coords(), (850, 15))
             pygame.display.flip()
-            pygame.display.update()
-
-
-
 
 
 screen.set_alpha(None)
@@ -452,5 +482,6 @@ rock = pygame.transform.scale(back, (r.blocksize, r.blocksize))
 back = pygame.transform.scale(back, (r.width, r.height))
 win = pygame.transform.scale(win, (r.width, r.height))
 lose = pygame.transform.scale(lose, (r.width, r.height))
+pause = pygame.transform.scale(pause, (r.width, r.height))
 clock = pygame.time.Clock()
 r.main_menu()
